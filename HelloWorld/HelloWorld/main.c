e2c5414f7fa46475d22ef9dc93fe66d739495ebc
Mallé <269384@via.dk>
2018-04-10 09:52:15 +0200

    change the program so that led 3 is turned on instead

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -6,5 +6,5 @@
 int main(void)
 {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(1<<PA0);      // turning on led 0 (active low)
+    PORTA = ~(1<<PA3);      // turning on led 0 (active low)
 }
